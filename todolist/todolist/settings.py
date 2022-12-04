f5b54a881cb0ca98a4c0da86f333841f270cea81
DoubleCitizen <500maxim005@gmail.com>
2022-12-04 12:44:12 +0700

    Test

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -25,7 +25,6 @@ environ.Env.read_env(ENV_FILE_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-#
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
