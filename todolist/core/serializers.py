4932a01269753b6cc3b554ed366a9390478e5cbb
DoubleCitizen <500maxim005@gmail.com>
2022-12-04 16:38:51 +0700

    Добавлено изменение пароля

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -4,7 +4,6 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed
 
-
 USER_MODEL = get_user_model()  # get User model
 
 
@@ -62,3 +61,32 @@ class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = USER_MODEL
         fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = USER_MODEL
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
+class UpdatePasswordSerializer(serializers.Serializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault)
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        user = attrs['user']
+        if not user.check_password(attrs['old_password']):
+            raise serializers.ValidationError({'old_password': 'incorrect password'})
+
+        try:
+            validate_password(attrs['new_password'])
+        except Exception as e:
+            raise serializers.ValidationError({'new_password': e.messages})
+
+        return attrs
+
+    def update(self, instance, validated_data):
+        instance.password = make_password(validated_data['new_password'])
+        instance.save()
+        return instance
--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import get_user_model
-from rest_framework import generics
+from django.contrib.auth import get_user_model, login, logout
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer, ProfileSerializer, UpdatePasswordSerializer
 
 USER_MODEL = get_user_model()
 
@@ -9,3 +10,35 @@ USER_MODEL = get_user_model()
 class RegistrationView(generics.CreateAPIView):
     model = USER_MODEL
     serializer_class = RegistrationSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
+
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    queryset = USER_MODEL.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request=request)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
