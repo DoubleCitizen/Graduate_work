a7b1b9643dce5949b2cc031eb8d3cd8ad7f9bb9e
DoubleCitizen <500maxim005@gmail.com>
2022-12-10 21:27:41 +0700

    Добавлены комментарии, цели и категории

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'social_django',
+    'goals',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -156,3 +158,8 @@ SOCIAL_AUTH_VK_OAUTH2_SECRET = env.str('SOCIAL_AUTH_VK_OAUTH2_SECRET')
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/logged-in/'
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
